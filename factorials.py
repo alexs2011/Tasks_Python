893c3ea3b401de84a159445c7084e64d2d6fe519
Alexander <kir-al93@yandex.ru>
2021-08-03 18:39:44 +0700

    Удаление отладочного кода

--- factorials.py
+++ factorials.py
@@ -16,23 +16,13 @@ n = 10_000_000
 
 for i in range(0, 10):
     factorials.append(math.factorial(i))
-# factorials = tuple(factorials)
 
-# t1 = time.time()
-# for number in range(n):
-#     if sum_factorials_of_digits(number) == number:
-#         res.append(number)
-# t2 = time.time()
-# print(t2 - t1)
-# print(res)
-
-# res.clear()
 t_beg = time.time()
 cached_sum = 0
 for number in range(n):
     last_digit = number % 10
     if last_digit == 0:
-        cached_sum = sum_factorials_of_digits(number//10)
+        cached_sum = sum_factorials_of_digits(number // 10)
         s = cached_sum
     else:
         s = cached_sum + factorials[last_digit]
