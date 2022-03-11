49a54ccc9456505999d5ec12b103bd353fe4f759
Alexander <kir-al93@yandex.ru>
2022-03-11 22:23:27 +0700

    Added 05 task solution

---
+++ SkyPro/Урок 6.2. Задания со звездочкой/task_5/05.py
@@ -0,0 +1,30 @@
+"""
+Лидар
+В файле furry_road.txt указываются плитки дороги шириной 5 клеток. Напишите автопилот, который едет сверху вниз и
+на каждый ряд выбирает, куда ехать: left, right, stay. При наличии нескольких вариантов машина держится правой стороны
+(на картинке она слева). Программа должна считывать файл построчно.
+"""
+
+obstacle = u'\u2593'
+clear_path = u'\u2591'
+
+car_pos_idx = 2
+
+print(f'  X   {car_pos_idx + 1}')
+
+with open('furry_road.txt', 'r', encoding='utf-8') as f_in:
+    for row in f_in:
+        row = row.strip().split()
+        print(''.join(row), end=' ')
+        if row[car_pos_idx] == obstacle:
+            if car_pos_idx > 0 and row[car_pos_idx - 1] == clear_path:
+                print(f'right->{car_pos_idx}')
+                car_pos_idx -= 1
+            elif car_pos_idx < len(row) - 1 and row[car_pos_idx + 1] == clear_path:
+                print(f'left->{car_pos_idx + 2}')
+                car_pos_idx += 1
+            else:
+                print('stop')
+                break
+        else:
+            print('stay')
