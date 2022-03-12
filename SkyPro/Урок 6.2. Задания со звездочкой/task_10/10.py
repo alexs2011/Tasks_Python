7eb7db6b6c7a0e80263be4da20efc04f4fcd31d6
Alexander <kir-al93@yandex.ru>
2022-03-12 14:03:29 +0700

    Added 10 task solution

--- SkyPro/Урок 6.2. Задания со звездочкой/task_10/10.py
+++ SkyPro/Урок 6.2. Задания со звездочкой/task_10/10.py
@@ -10,4 +10,12 @@
 
 import csv
 
+d = {}
 
+with open('score.csv', 'r', encoding='utf-8') as f_in:
+    reader = csv.reader(f_in)
+    for row in reader:
+        d[row[0]] = sum(map(int, row[1:])) / 3
+
+for name, val in d.items():
+    print(f'{name}: {val:.1f}')
