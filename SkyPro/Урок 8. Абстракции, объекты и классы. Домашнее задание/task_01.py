06454f660fa23fd9a0892fe0cc9b6b7408005879
Alexander <kir-al93@yandex.ru>
2022-03-24 15:20:41 +0900

    Fix typo in 08 hw task solution

--- SkyPro/Урок 8. Абстракции, объекты и классы. Домашнее задание/task_01.py
+++ SkyPro/Урок 8. Абстракции, объекты и классы. Домашнее задание/task_01.py
@@ -98,13 +98,13 @@ if __name__ == '__main__':
     user_input = input("Задавать ли вопросы в случайном порядке (y/n): ").lower()
     if user_input == 'y':
         is_mode_random = True
-        avaliable_questions_idx = list(range(len(questions)))
+        available_questions_idx = list(range(len(questions)))
 
     for i in range(len(questions)):
         cur_question_idx = i
         if is_mode_random:
-            cur_question_idx = random.choice(avaliable_questions_idx)
-            avaliable_questions_idx.remove(cur_question_idx)
+            cur_question_idx = random.choice(available_questions_idx)
+            available_questions_idx.remove(cur_question_idx)
 
         question = questions[cur_question_idx]
 
