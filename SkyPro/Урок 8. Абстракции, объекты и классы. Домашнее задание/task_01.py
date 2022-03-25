15d5a4b36fe7023c908c6d065d3256be136aff57
Alexander <kir-al93@yandex.ru>
2022-03-25 10:55:29 +0900

    Improved 08 hw task solution

--- SkyPro/Урок 8. Абстракции, объекты и классы. Домашнее задание/task_01.py
+++ SkyPro/Урок 8. Абстракции, объекты и классы. Домашнее задание/task_01.py
@@ -6,20 +6,11 @@ if __name__ == '__main__':
 
     questions = utils.load_questions()
 
-    is_mode_random = False
-
     user_input = input("Задавать ли вопросы в случайном порядке (y/n): ").lower()
     if user_input == 'y':
-        is_mode_random = True
-        available_questions_idx = list(range(len(questions)))
+        random.shuffle(questions)
 
-    for i in range(len(questions)):
-        cur_question_idx = i
-        if is_mode_random:
-            cur_question_idx = random.choice(available_questions_idx)
-            available_questions_idx.remove(cur_question_idx)
-
-        question = questions[cur_question_idx]
+    for question in questions:
 
         print(f"\n{question.build_question()}")
 
