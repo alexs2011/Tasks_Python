4620527ebfcb660d466f1f8359c78b36c8c81fb1
Alexander <kir-al93@yandex.ru>
2022-04-11 11:13:21 +0900

    Removed unnecessary comments

--- coloredmanga_scrapper/coloredmanga_scrapper_async/utility/async_pages_downloader.py
+++ coloredmanga_scrapper/coloredmanga_scrapper_async/utility/async_pages_downloader.py
@@ -1,6 +1,5 @@
 import aiohttp
 import asyncio
-import aiofiles
 
 import utility.utils as utils
 from classes.page import Page
@@ -11,10 +10,6 @@ async def get_page(session: aiohttp.ClientSession, page: Page) -> None:
     На основе url-адреса асинхронно скачивает изображение и сохраняет его.
     """
     async with session.get(page.url) as resp:
-        # if resp.status == 200:
-        #     f = await aiofiles.open(page.path, mode='wb')
-        #     await f.write(await resp.read())
-        #     await f.close()
         with open(page.path, 'wb') as fd:
             async for chunk in resp.content.iter_chunked(1024 * 8):
                 fd.write(chunk)
