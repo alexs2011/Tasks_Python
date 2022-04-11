fcfe4bbbbeb16269467fe9bdc698e4140909de8d
Alexander <kir-al93@yandex.ru>
2022-04-11 10:49:21 +0900

    Added new version of курсовая1

--- SkyPro/Помощь/Валента/курсовая1.py
+++ SkyPro/Помощь/Валента/курсовая1.py
@@ -1,23 +1,88 @@
-list = {"code": "-.-. --- -.. .",
-"bit":  "-... .. -",
-"list":  ".-.. .. ... -",
-"soul" : "... --- ..- .-..",
-"next" : "-. . -..- -"}
+import code
+import random
 
-#print("Сегодня мы потренируемся расшифровывать азбуку Морзе"
-"Нажмите Enter и начнем"
-answers = []
+import bit as bit
 
-def morse_encode(code):
-    pass
-def morse_encode(bit):
-    pass
-def morse_encode(list):
-    pass
-def morse_encode(soul):
-    pass
-def morse_encode(next):
-    pass
+morse_code = {
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "!": "-.-.--",
+    "-": "-....-",
+    "/": "-..-.",
+    "@": ".--.-.",
+    "(": "-.--.",
+    ")": "-.--.-"
+}
+
+print("Сегодня мы потренируемся расшифровывать морзянку.Нажмите Enter и начнем")
+
+english_code = [cat, horse, run, beer]
+answer = []
+
+
+def print_statistics(english_code):
+    """Подсчитываем правильные/неправильные ответы"""
+
+    if answer in english_code:
+        print("Отвечено верно")
+    else:
+        print("Отвечено неверно")
+
+    return f"Всего задачек: 4, \n Отвечено верно: {english_code}, \n Отвечено неверно: {}"
+
+
+
+def morse_encode(word):
+    """Переводим с збуки морзе на английский"""
+
+    word = []
+
+    for char in word:
+        word.append(morse_code[char])
+    return word
+
 
 def get_word():
-    bit
+    """Получаем случайное слово из списка"""
+    english_code = [cat, horse, run, beer]
+    items = random.sample(english_code, 1)
+    result = ' '.join(items)
+
+    print(result)
